813aabb597f96290462e86acf960ef55cc31a006
Akmal Sindorov <asakew@ya.ru>
2021-01-17 14:38:45 +0500

    Update head.php

--- head.php
+++ head.php
@@ -5,5 +5,4 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo site_title; ?></title>
-<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
+<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
