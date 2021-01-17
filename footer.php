5e317814d3df130215d6d8a44e99e1c43093c708
Akmal Sindorov <asakew@ya.ru>
2021-01-17 14:38:21 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<footer>
+<footer class="m-3">
 	<?php
 echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
 ?>
 </footer>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/js/bootstrap.bundle.min.js"></script>
